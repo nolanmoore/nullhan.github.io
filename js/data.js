c271398ec877c32454e242a41a720e6122cd3f6d
Nolan M <nolan.moore@gmail.com>
2017-02-27 16:03:29 -0800

    added new images, changed theme to blue, fixed project grid for desktop and responsive

--- js/data.js
+++ js/data.js
@@ -2,6 +2,7 @@ const data = {
   "projects": [
     {
       "type": "hardware",
+      "status": "Complete",
       "title": "Luci",
       "info": "The Luci flight controller is designed to give drone developers a platform on which to easily create rich IoT-enabled applications. Luci is powered by the PX4 flight stack as well as an Intel Edison co-processor that hosts the user's application and provides Wi-Fi connectivity.",
       "client": "Dronesmith Technologies",
@@ -18,6 +19,7 @@ const data = {
     },
     {
       "type": "hardware",
+      "status": "Complete",
       "title": "Luci Development Expansion Board",
       "info": "A daughterboard that gives developers access to the expansion connector underneath the Luci flight controller. Also provides additional serial ports for the Intel Edison co-processor and a slot for an optional Skywire LTE modem.",
       "client": "Dronesmith Technologies",
@@ -34,6 +36,7 @@ const data = {
     },
     {
       "type": "hardware",
+      "status": "Complete",
       "title": "Quad Electronic Speed Controller for Brushless DC Motors",
       "info": "A motor controller board designed to power four brushless DC motors at up to 20A each. Designed to be used in quadcopters, especially the Dronesmith Technologies QuaRK.",
       "client": "Dronesmith Technologies",
@@ -42,11 +45,14 @@ const data = {
       "categories": [
         "UAV development"
       ],
-      "images": [],
-      "previewImage": "quadesc_placeholder.png"
+      "images": [
+        "quadesc_v22_trans.png"
+      ],
+      "previewImage": "quadesc_v22_trans.png"
     },
     {
       "type": "hardware",
+      "status": "WIP",
       "title": "Power Glove Controlled Drone",
       "info": "An overhaul of the iconic Power Glove to allow it to control robots, drones or anything your heart desires. It's so... good.<br />Features custom boards designed to completely replace the original electronics. <a href=\"https://hackaday.io/project/8455-power-glove-universal-hid\">Project on Hackaday.io</a>",
       "client": "Personal project",
@@ -65,6 +71,7 @@ const data = {
     },
     {
       "type": "hardware",
+      "status": "WIP",
       "title": "IoT Alarm Clock",
       "info": "A small project featuring a Wi-Fi enabled snooze button and a Python-based server, all linked by the MQTT messaging protocol.",
       "client": "Personal project",
@@ -79,6 +86,7 @@ const data = {
     },
     {
       "type": "software",
+      "status": "WIP",
       "title": "Happy Hour Finder",
       "info": "A MEAN stack powered webapp that locates the bars nearest to the user and determines which currently have happy hour specials. Data pulled from Yelp using the Fusion API.<br /><a href=\"http://happyhour.nolandoesdev.com/\">Hosted on Heroku</a>",
       "client": "Personal project",
@@ -87,11 +95,15 @@ const data = {
       "categories": [
         "Web development"
       ],
-      "images": [],
+      "images": [
+        "happyhour.png",
+        "happyhour_ss.png"
+      ],
       "previewImage": "happyhour.png"
     },
     {
       "type": "software",
+      "status": "WIP",
       "title": "Half Past Brew Webapp",
       "info": "A MEAN-stack based webapp designed to find breweries and taprooms near the user and suggest a beer from each. Data provided by <a href=\"http://brewerydb.com\">BreweryDB.com</a>",
       "client": "Personal project",
